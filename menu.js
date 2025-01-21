48e45bf26c9d31043cc18a079e6b65e510774245
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-01-21 12:17:50 +0100

    Fått til å skrive ut kategori.
    Generert JS-kode for å få ut kategorinavn på de ulike fanene.

---
+++ menu.js
@@ -0,0 +1,19 @@
+
+let informasjonHTML = ""
+
+resources.map((cat, index) => {
+    informasjonHTML += `<button>${cat.category}</button>`
+
+})
+
+document.getElementById("menu").innerHTML = informasjonHTML
+
+
+
+
+
+
+
+
+
+

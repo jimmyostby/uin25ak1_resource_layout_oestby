79b8709fe452df5bbbde08f3921dba35f16b9af5
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-01-22 21:01:20 +0100

    testet
    Testet ulike varianter for å få skrevet ut informasjon under de ulike menyvalgene. Fikk det ikke til.

--- menu.js
+++ menu.js
@@ -1,11 +1,35 @@
 
 let informasjonHTML = ""
 
-resources.map((cat, index) => {
-    informasjonHTML += `<button>${cat.category}</button>`
 
-})
-document.getElementById("menu").innerHTML = informasjonHTML
+/*
+resources.map((cat, index) => {
+    informasjonHTML += `<button onclick="content('${cat.category}')">${cat.category}</button>`})
+
+    */ 
+
+    resources.map(resource => {
+        informasjonHTML += `<button id="button" onclick="updatecontent('${resource.category}')">${resource.category}</button>`;
+      });
+      
+
+function content(category){
+    const filter = resources.filter (item => item.category === category)
+    const clicked = filter [0]
+    const information = `<h2>${clicked.category}</h2>`
+
+    document.getElementById("menu").innerHTML=information
+    }
+    
+    content("HTML")
+
+
+
+
+
+
+
+
 
 
 

bd16aef2b3a19207e727d67feced9ca9cd5a8757
Jimmy Østby <142933101+jimmyostby@users.noreply.github.com>
2025-01-23 19:30:39 +0100

    Slettet og endret.
    Slettet og endret på kode. Feilsøkte koden ved hjelp av chat-gpt og rettet opp forestående feil. Får nå skrevet ut "knappene" til nettsiden.

--- menu.js
+++ menu.js
@@ -1,15 +1,15 @@
 let informasjonHTML = ""
 
-resources.map(resource => informasjonHTML += 
+resources.map(resource => informasjonHTML += `
             <ul id="menu">
                 <li>
                     <p id="menu" onclick="content('${resource.category}')" >${resource.category}</p>
                 </li>
             </ul>
-)
+
+`)
 
 document.getElementById("menu").innerHTML = informasjonHTML
-
 
 function content(category){
     const filter = resources.filter (item => item.category === category)
@@ -19,13 +19,14 @@ function content(category){
         <p id="text">${clicked.text}</p>
         <ul>
             ${clicked.sources.map(source => 
-                <li><a href="${source.url}" target="_blank">${source.title}</a></li>
+                `<li><a href="${source.url}" target="_blank">${source.title}</a></li>`
             ).join("")}
         </ul>
-    `
-
+        `
+        
 
     document.getElementById("content").innerHTML=informationHTML
     }
 
 content("HTML")
+
